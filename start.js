05aba113bf2e661d58882e126b9bbdc6ef9b7085
Tommy <54972963+EWCMF@users.noreply.github.com>
2020-10-25 16:02:07 +0100

    docker

--- start.js
+++
@@ -1,6 +0,0 @@
-var functions = require('./public/javascripts/functions');
-
-console.log(functions.add(1, 2));
-console.log(functions.sub(5, 6));
-console.log(functions.mul(6, 7));
-console.log(functions.div(7, 7));
